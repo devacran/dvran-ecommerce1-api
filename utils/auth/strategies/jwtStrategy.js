41fe296f6ca47ab9736ee31cdb4a76c29ad9da68
Miguel Angel de la Rosa Guerrero <migueeldrg@gmail.com>
2022-10-15 17:47:34 -0500

    feat: add localhost url to cors

--- utils/auth/strategies/jwtStrategy.js
+++ utils/auth/strategies/jwtStrategy.js
@@ -4,7 +4,6 @@ const options = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
   secretOrKey: config.jwtSecret,
 };
-
 const JwtStrategy = new Strategy(options, (payload, done) => {
   return done(null, payload);
 });
